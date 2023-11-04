c42b72978f4758d49df9164e4777e12286271268
DML <dmlinh2003@gmail.com>
2023-11-05 00:23:35 +0700

    Add file demo01_p5.php

---
+++ demo01_p5.php
@@ -0,0 +1,14 @@
+<?php
+    echo "<h1> GIT BRANCH LÀ GÌ? </h1>";
+    echo "<p> Các Branch (nhánh) đại diện cho các phiên bản cụ thể của một kho lưu trữ tách ra từ project chính của bạn.</p>";
+    echo " Branch cho phép bạn theo dõi các thay đổi thử nghiệm bạn thực hiện đối với kho lưu trữ và có thể hoàn nguyên về các phiên bản cũ hơn.";
+    echo "<h2> Các thao tác trên GIT BRANCH.</h2>";
+    echo "<h3> 5.1. Tạo nhánh mới: </h3>";
+    echo "<p> git branch [branch_name]</p>";
+    echo "<h3> 5.2. Chuyển đổi nhánh: </h3>";
+    echo "<p> git checkout [branch_name]</p>";
+    echo "<h3> 5.3. Xem nhánh hiện tại và danh sách nhánh: </h3>";
+    echo "<p> git branch -l</p>";
+    echo "<h3> 5.4. Xóa nhánh: </h3>";
+    echo "<p> git branch -d [branch_name]</p>";
+?>
