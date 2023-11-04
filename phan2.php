a2c6f1c8e8330a6a9e4ea2abbebb29fe802ae642
unknown <tamhagiangngoc@gmail.com>
2023-11-04 22:30:39 +0700

    Thêm tệp phan2.php

---
+++ phan2.php
@@ -0,0 +1,5 @@
+<?php 
+    echo 
+    "Git là một hệ thống quản lý phiên bản (Version Control System - VCS) phổ biến được sử dụng rộng rãi trong phát triển phần mềm. 
+    Hệ thống quản lý phiên bản giúp theo dõi và quản lý sự thay đổi trong mã nguồn và tài liệu dự án."
+?>
